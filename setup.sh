d78619023fa37cc629503d5edd93002b07ccc44c
Ali Sarchami <ali.sarchami@gmail.com>
2017-11-25 13:28:16 -0600

    update folder structure

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+rm -rf ~/.vim* 
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 curl -fLo ~/.vim/colors/molokai.vim --create-dirs \
         https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
 
-rm -rf ~/.vim* ~/.myvim
 cp vimrc ~/.vimrc
 vim +PlugInstall +qall
+rm -rf ~/.myvim
