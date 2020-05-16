46fa201c0e546917c6c4f4ab966c77e0182da565
Ali Sarchami <ali.sarchami@gmail.com>
2020-05-16 14:06:19 +0000

    Install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#/usr/bin/bash
+
+git clone https://github.com/asarchami/dotfiles.git
+rm -rf ~/.config/nvim
+mkdir ~/.config/nvim
+cp dotfiles/*.vim ~/.config/nvim
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cp tmux.conf ~/.tmux.conf
+rm -rf dotfiles
