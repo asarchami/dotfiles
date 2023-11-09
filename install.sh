57ebfa6dae19388a742a71f676b0b14759856816
Ali Sarchami <ali.sarchami@capeanalytics.com>
2023-11-09 12:18:36 -0600

    update install - WIP

--- install.sh
+++ install.sh
@@ -5,17 +5,26 @@ set -u
 set -o pipefail
 
 is_app_installed() {
-  type "$1" &>/dev/null
+	type "$1" &>/dev/null
 }
 
 if ! is_app_installed tmux; then
-  printf "WARNING: \"tmux\" command is not found. \
+	printf "Error: \"tmux\" command is not found. \
 Install it first\n"
-  exit 1
+	exit 1
+
+fi
+if ! is_app_installed nvim; then
+	printf "Error: \"nvim\" command is not found. \
+Install it first\n"
+	exit 1
+	nvim_link="https://github.com/neovim/neovim/releases/download/stable/nvim.appimage"
+	mkdir -p ~/.local/bin
+	curl -Lo ~/.local/bin/nvim.appimage $nvim_link && chmod +x ~/.local/bin/nvim.appimage && sudo cp .local/bin/nvim.appimage /usr/local/bin/nvim
 fi
 
 rm -rf dotfiles
-git clone  https://github.com/asarchami/dotfiles.git
+git clone https://github.com/asarchami/dotfiles.git
 rm -rf ~/.config/nvim-back && mv ~/.config/nvim ~/.config/nvim-back || true
 rm -rf ~/.tmux && mv ~/.tmux ~/.tmux-back || true
 rm -rf ~/.tmux.conf && mv ~/.tmux.conf ~/.tmux.conf-back || true
