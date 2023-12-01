738dd03f81f471c4ac3ab7fbe7132adaa648e8e4
Ali Sarchami <ali.sarchami@capeanalytics.com>
2023-12-01 15:50:11 -0600

    organize ui stuff

---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,55 @@
+return {
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      style = "darker",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+        window = {
+          mappings = {
+            ["L"] = "open_nofocus",
+          },
+        },
+        commands = {
+          open_nofocus = function(state)
+            require("neo-tree.sources.filesystem.commands").open(state)
+            vim.schedule(function()
+              vim.cmd([[Neotree focus]])
+            end)
+          end,
+        },
+      },
+      window = {
+        mappings = {
+          ["l"] = "open",
+          ["h"] = "close_node",
+        },
+      },
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function(file_path)
+            -- auto close
+            -- vimc.cmd("Neotree close")
+            -- OR
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+      },
+    },
+  },
+}
