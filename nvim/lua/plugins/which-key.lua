f52cc5aea21939674ce3177ed8fba7461c5cd345
Ali Sarchami <ali.sarchami@capeanalytics.com>
2024-02-04 15:21:32 -0600

    added names for missing groups

---
+++ nvim/lua/plugins/which-key.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>r"] = { name = "+Rest Client" },
+        ["<leader>n"] = { name = "+NeOrg" },
+        ["<leader>nj"] = { name = "+Journal" },
+        ["<leader>nt"] = { name = "+Tasks" },
+        ["<leader>nn"] = { name = "+Notes" },
+        ["<leader>wd"] = { name = "+Delete" },
+        ["<localleader>i"] = { name = "+Insert" },
+      },
+    },
+  },
+}
