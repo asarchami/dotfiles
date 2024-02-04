40fb1bf44a794edceef3ab333cccf669698b353e
Ali Sarchami <ali.sarchami@capeanalytics.com>
2024-02-04 15:20:27 -0600

    shortcuts
    use vip.keymap.set instead of Util.safe_keymap_set
    wda to delete all buffers and close all windows except one

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local Util = require("lazyvim.util")
-local map = Util.safe_keymap_set
+local map = vim.keymap.set
 local lazyterm = function()
   Util.terminal(nil, { cwd = Util.root(), border = "single" })
 end
@@ -12,3 +12,11 @@ map("n", "<leader>fT", function()
 end, { desc = "Terminal (cwd)" })
 map("n", "<c-/>", lazyterm, { desc = "Terminal (root dir)" })
 map("n", "<c-_>", lazyterm, { desc = "which_key_ignore" })
+map("n", "<leader>wda", function()
+  vim.cmd([[bufdo bw!]])
+  vim.cmd([[only]])
+end, { desc = "Delete all buffers and windows" })
+map("n", "<leader>wdd", "<cmd>q<CR>", { desc = "Delete current windows" })
+-- map("n", "gC", function()
+--   require("treesitter-context").go_to_context(vim.v.count1)
+-- end, { silent = true, desc = "Go to context" })
