ad7bc09084ab1554b422f8db3dcc073fb81a92cc
Ali Sarchami <ali.sarchami@capeanalytics.com>
2024-04-07 15:16:26 -0500

    better organized

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -8,4 +8,58 @@ return {
       trim_first_line = true,
     },
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+        window = {
+          mappings = {
+            ["L"] = "open_nofocus",
+          },
+        },
+        commands = {
+          open_nofocus = function(state)
+            require("neo-tree.sources.filesystem.commands").open(state)
+            vim.schedule(function()
+              vim.cmd([[Neotree focus]])
+            end)
+          end,
+        },
+      },
+      window = {
+        mappings = {
+          ["l"] = "open",
+          ["h"] = "close_node",
+        },
+      },
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function(file_path)
+            -- auto close
+            -- vimc.cmd("Neotree close")
+            -- OR
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+      },
+    },
+  },
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      style = "darker",
+    },
+  },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
 }
