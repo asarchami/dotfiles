19c1f9c618ff7c00065f7169b6547de7baad9a0d
Ali Sarchami <ali.sarchami@gmail.com>
2025-11-08 21:24:59 -0600

    feat(yazi): add separate install script for yazi and deps

---
+++ dot_config/yazi/run_once_install-yazi-deps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script is run once by chezmoi to install yazi dependencies.
+
+set -e
+
+# Get the directory of this script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Run the main installation script
+"$SCRIPT_DIR/../../install_yazi_deps.sh"
