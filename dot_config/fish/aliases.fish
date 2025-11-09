08591aef61884469c4018876fc9e47e3346c8ce7
Ali Sarchami <ali.sarchami@gmail.com>
2025-11-09 16:43:04 -0600

    refactor: Modularize fish aliases and remove unused completions
    - Removed `dot_config/fish/completions` as `lf` is not used.
    - Moved Git and other aliases from `config.fish` to `aliases.fish` for better organization.

---
+++ dot_config/fish/aliases.fish
@@ -0,0 +1,15 @@
+# Aliases
+abbr g git
+abbr gst 'git status'
+abbr ga 'git add'
+abbr gaa 'git add --all'
+abbr gc 'git commit -v'
+abbr gcm 'git commit -m'
+abbr gco 'git checkout'
+abbr gb 'git branch'
+abbr gp 'git push'
+abbr gl 'git pull'
+abbr gd 'git diff'
+abbr glog 'git log --oneline --decorate --graph'
+abbr c clear
+alias vim nvim
