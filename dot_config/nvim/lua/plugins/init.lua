39f07d28a067b3db0149eecb9d528c69f46021ae
Ali Sarchami <ali.sarchami@gmail.com>
2025-12-23 19:29:05 -0600

    feat: add seamless tmux/neovim navigation with vim-tmux-navigator
    - Add vim-tmux-navigator plugin to neovim
    - Configure tmux keybindings for C-h/j/k/l pane navigation
    - Enable smart detection to route keys to vim or tmux appropriately

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -13,5 +13,6 @@ vim.list_extend(plugins, require("plugins.which-key"))
 vim.list_extend(plugins, require("plugins.noice"))
 vim.list_extend(plugins, require("plugins.surround"))
 vim.list_extend(plugins, require("plugins.markdown"))
+vim.list_extend(plugins, require("plugins.navigator"))
 
 return plugins
