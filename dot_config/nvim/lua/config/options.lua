aa5c34de2e63a0292470aef1314f85f3c9df2c54
Ali Sarchami <ali.sarchami@gmail.com>
2025-12-24 13:48:02 -0600

    Update nvim and tmux configs, add lazygit configuration

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -44,5 +44,5 @@ vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldcolumn = "0"
 vim.opt.foldtext = ""
 vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 1
-vim.opt.foldnestmax = 4
+-- vim.opt.foldlevelstart = 2
+-- vim.opt.foldnestmax = 6
