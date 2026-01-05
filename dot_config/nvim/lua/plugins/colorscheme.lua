3f15c4ee8ed1666aab539dba8be4d05e60d17fc8
Ali Sarchami <ali.sarchami@gmail.com>
2026-01-05 17:09:58 -0600

    Disable bufferline tabs in neovim

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,7 @@
 return {
-  -- add gruvbox
   {
-    "ellisonleao/gruvbox.nvim",
-    opts = {
-      transparent_mode = true,
-    },
-  },
-
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon", transparent = true },
   },
 }
