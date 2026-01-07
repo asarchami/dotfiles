ab3a96433b0f2afc54c7f1079b4d29a1caf37a1b
Ali Sarchami <sarchama@moodys.com>
2026-01-07 10:59:25 -0600

    Update snacks explorer configuration (attempt to add Ctrl+h for horizontal split)

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -6,4 +6,21 @@ return {
     "akinsho/bufferline.nvim",
     enabled = false,
   },
+  -- Snacks explorer: Change Ctrl+h to open file in horizontal split (replacing Ctrl+s)
+  {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      explorer = {},
+      picker = {
+        sources = {
+          explorer = {
+            keys = {
+              ["<C-h>"] = "edit_split",
+            },
+          },
+        },
+      },
+    },
+  },
 }
