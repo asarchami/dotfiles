da8cf7bb59726c0a13c738698db7f1ff164e28a0
Ali Sarchami <sarchama@moodys.com>
2026-01-08 08:21:47 -0600

    Update neovim keymaps configuration

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,10 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
--- vim.keymap.del("n", "<leader>wh")
--- vim.keymap.del("n", "<leader>wl")
--- vim.keymap.del("n", "<leader>wk")
--- vim.keymap.del("n", "<leader>wj")
-
+--
 -- local map = vim.keymap.set
 -- map("n", "<leader>wh", "<cmd>split<cr>", { desc = "Split window horizontally" })
