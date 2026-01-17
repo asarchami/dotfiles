92604d589e86c24178a77b18cfbe5a27d5c80a02
Ali Sarchami <ali.sarchami@gmail.com>
2026-01-16 20:13:46 -0600

    Integrate yazi.nvim as file manager with custom keybindings
    - Add yazi.nvim plugin with <leader>e as primary keybinding
    - Configure custom keybindings: ?=help, c-s=horizontal split, c-f=grep
    - Disable Snacks explorer to avoid conflicts
    - Support both normal and visual mode file selection

---
+++ dot_config/nvim/lua/plugins/file-manager.lua
@@ -0,0 +1,36 @@
+-- Yazi file manager integration
+return {
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  keys = {
+    {
+      "<leader>e",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      "<leader>cw",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open yazi in the current working directory",
+    },
+    {
+      "<c-up>",
+      "<cmd>Yazi toggle<cr>",
+      desc = "Resume the last yazi session",
+    },
+  },
+  opts = {
+    open_for_directories = false,
+    floating_window_scaling_factor = 0.9,
+    yazi_floating_window_border = "rounded",
+    keymaps = {
+      show_help = "?",
+      open_file_in_vertical_split = "<c-v>",
+      open_file_in_horizontal_split = "<c-s>",
+      open_file_in_tab = "<c-t>",
+      grep_in_directory = "<c-f>",
+      send_to_quickfix_list = "<c-q>",
+    },
+  },
+}
