b1df6c6c95bb65ef3f923481a5d0b2c92e6341c1
Ali Sarchami <sarchama@moodys.com>
2026-08-26 10:05:06 -0500

    style(yazi): add file-explorer icon to <leader>e/<leader>E

--- dot_config/nvim/lua/plugins/file-manager.lua
+++ dot_config/nvim/lua/plugins/file-manager.lua
@@ -1,36 +1,48 @@
 -- Yazi file manager integration
 return {
-  "mikavilpas/yazi.nvim",
-  event = "VeryLazy",
-  keys = {
-    {
-      "<leader>e",
-      mode = { "n", "v" },
-      "<cmd>Yazi<cr>",
-      desc = "Open yazi at the current file",
+  {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>e",
+        mode = { "n", "v" },
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
+      },
+      {
+        "<leader>E",
+        "<cmd>Yazi cwd<cr>",
+        desc = "Open yazi in the current working directory",
+      },
+      {
+        "<c-up>",
+        "<cmd>Yazi toggle<cr>",
+        desc = "Resume the last yazi session",
+      },
     },
-    {
-      "<leader>E",
-      "<cmd>Yazi cwd<cr>",
-      desc = "Open yazi in the current working directory",
-    },
-    {
-      "<c-up>",
-      "<cmd>Yazi toggle<cr>",
-      desc = "Resume the last yazi session",
+    opts = {
+      open_for_directories = false,
+      floating_window_scaling_factor = 0.9,
+      yazi_floating_window_border = "rounded",
+      keymaps = {
+        show_help = "?",
+        open_file_in_vertical_split = "<c-v>",
+        open_file_in_horizontal_split = "<c-s>",
+        open_file_in_tab = "<c-t>",
+        grep_in_directory = "<c-f>",
+        send_to_quickfix_list = "<c-q>",
+      },
     },
   },
-  opts = {
-    open_for_directories = false,
-    floating_window_scaling_factor = 0.9,
-    yazi_floating_window_border = "rounded",
-    keymaps = {
-      show_help = "?",
-      open_file_in_vertical_split = "<c-v>",
-      open_file_in_horizontal_split = "<c-s>",
-      open_file_in_tab = "<c-t>",
-      grep_in_directory = "<c-f>",
-      send_to_quickfix_list = "<c-q>",
+  {
+    "folke/which-key.nvim",
+    optional = true,
+    opts = {
+      spec = {
+        { "<leader>e", mode = { "n", "v" }, icon = { icon = "", color = "yellow" } },
+        { "<leader>E", icon = { icon = "", color = "yellow" } },
+      },
     },
   },
 }
