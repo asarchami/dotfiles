2246ee1df820ac3c5766ea0d129f9c330fc05fab
Ali Sarchami <sarchama@moodys.com>
2026-09-02 15:41:18 -0500

    feat(nvim): install annotate-markdown.nvim from GitHub
    Local dir= install was for early development; the plugin is now pushed to
    git@github-personal:asarchami/annotate-markdown.nvim.git.

---
+++ dot_config/nvim/lua/plugins/annotate-markdown.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "asarchami/annotate-markdown.nvim",
+    url = "git@github-personal:asarchami/annotate-markdown.nvim.git",
+    ft = "markdown",
+    cmd = "AnnotateMarkdown",
+  },
+}
